0454b42cd7e1f5df919f7d2e82546ffed6f7fe16
dtswanson <dtswanson@gmail.com>
2024-12-11 12:58:55 +0100

    Updated with branding.

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+import random
+import sqlite3
+
 
 app = Flask(__name__)
 
-
+def get_random_recommendation():
+    conn = sqlite3.connect('recommendations.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT data, audience FROM recommendations ORDER BY RANDOM() LIMIT 1')
+    row = cursor.fetchone()
+    conn.close()
+    if row:
+        return {"recommendation": row[0], "audience": row[1]}
+    return None
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+@app.route('/recommendation')
+def recommendation():
+    recommendation = get_random_recommendation()
+    if recommendation:
+        return jsonify({'recommendation': recommendation['recommendation'], 'audience': recommendation['audience']})
+    return jsonify({'error': 'No recommendations found'}), 404
 
 if __name__ == '__main__':
-    app.run()
+    from databaseINIT import init_db, populate_db
+    init_db()
+    populate_db()
+    app.run(debug=True)
